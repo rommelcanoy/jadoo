11cac96b5bb8e3683eddf0bc1b7924226c5c30bd
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-20 13:53:48 +0800

    chore: add initial categories component

---
+++ app/Categories.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import decore from '../public/assets/Decore.svg';
+import Image from 'next/image';
+
+const Categories = () => {
+  return (
+    <div>
+
+      <div>
+        <Image src={decore} alt="decore" />
+      </div>
+
+      Categories
+
+
+
+    </div>
+  )
+}
+
+export default Categories
