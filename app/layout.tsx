ac2d9b862b1d0965b3a8fdd67451a467c4826294
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-20 13:52:39 +0800

    chore: add open sans font as a general font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Open_Sans } from 'next/font/google'
+import TopNav from './TopNav'
+const open_sans = Open_Sans({ weight: ['300', '400', '500', '600', '700', '800'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`max-w-7xl mx-auto ${open_sans.className} font-medium`}>
+        <div>
+          <TopNav />
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
