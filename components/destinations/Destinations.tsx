146ec27c7102d56324219d8a8bd809e7c7e27559
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-22 22:07:33 +0800

    style: update new design of destination components

--- components/destinations/Destinations.tsx
+++ components/destinations/Destinations.tsx
@@ -8,10 +8,10 @@ const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ['latin
 
 const Destinations = () => {
   return (
-    <div className="text-center mt-28 pb-20">
+    <div className="text-center mt-28 pb-20 debig">
       <h3 className={`uppercase text-[#5E6282] text-lg font-semibold ${poppins.className}`}>Top Selling</h3>
-      <h2 className={`text-5xl font-bold text-[#14183E] mt-4 mb-8 ${volkhov.className}`}>Top Destinations</h2>
-      <div className='flex gap-12'>
+      <h2 className={`text-5xl font-bold text-[#14183E] mt-4 mb-14 ${volkhov.className}`}>Top Destinations</h2>
+      <div className='grid grid-cols-3 gap-12'>
         {
           destinations.map((destination, index) => (
             <Destination key={index} destination={destination} />
