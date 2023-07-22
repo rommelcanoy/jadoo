aaf806f88afa2155a6257bab9e4d401174376a76
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-22 22:06:15 +0800

    chore: add and update new assets

--- public/index.ts
+++ public/index.ts
@@ -1,14 +1,21 @@
+
+// Icons
+import navigation from '../public/assets/navigation.svg';
+
+
 // Categories
 import calculated_weather from '../public/assets/calculated_weather.svg';
 import best_flights from '../public/assets/best_flights.svg';
 import local_events from '../public/assets/local_events.svg';
 import customization from '../public/assets/customization.svg';
 
-import rome from '../public/assets/Rome.png';
-import london from '../public/assets/London.png';
-import europe from '../public/assets/Europe.png';
+// Destinations
+import rome from '../public/assets/Rome.jpg';
+import london from '../public/assets/London.jpg';
+import europe from '../public/assets/Europe.jpg';
 
 export {
+  navigation,
   calculated_weather,
   best_flights,
   local_events,
