8b6880f740f7f15d1e96fcff2df9a0794847c7f4
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-22 17:02:51 +0800

    chore: import destination images

--- public/index.ts
+++ public/index.ts
@@ -4,9 +4,16 @@ import best_flights from '../public/assets/best_flights.svg';
 import local_events from '../public/assets/local_events.svg';
 import customization from '../public/assets/customization.svg';
 
+import rome from '../public/assets/Rome.png';
+import london from '../public/assets/London.png';
+import europe from '../public/assets/Europe.png';
+
 export {
   calculated_weather,
   best_flights,
   local_events,
   customization,
+  rome,
+  london,
+  europe,
 }
