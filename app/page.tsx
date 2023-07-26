354e9e754ce34743ea3064cdc20f298454c2d26f
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-26 16:03:50 +0800

    style: import initial testimonial component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Destinations from '@/components/destinations/Destinations'
 import StepsGuide from '@/components/StepsGuide'
 import SubscriptionForm from '@/components/SubscriptionForm'
+import Testimonials from '@/components/testimonials/Testimonials'
 import Categories from '../components/categories/Categories'
 import Header from '../components/Header'
 
@@ -11,6 +12,7 @@ export default function Home() {
       <Categories />
       <Destinations />
       <StepsGuide />
+      <Testimonials />
       <SubscriptionForm />
     </div>
   )
