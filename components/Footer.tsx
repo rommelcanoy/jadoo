d9e4768b2d6fac0388a39730e4ca16a09f1ed9e6
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-28 22:52:11 +0800

    style: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { apple, available_in_apple_store, facebook, get_it_on_google_play, googl
 const Footer = () => {
   return (
     <div className='py-20'>
-      <div className='grid grid-cols-1 md:grid-cols-5 gap-10 text-center md:text-left'>
+      <div className='grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-10 text-center md:text-left'>
         <div className='hidden md:block'>
           <Image src={jadoo} alt="Jadoo Logo" className='w-[100px] mb-8' />
           <p>Book your trip in minute, get full control for much longer.</p>
@@ -38,7 +38,7 @@ const Footer = () => {
           <Image src={jadoo} alt="Jadoo Logo" className='w-[100px] mb-8 mx-auto' />
           <p className=' text-sm'>Book your trip in minute, get full control for much longer.</p>
         </div>
-        <div className='md:w-[214px] w-full flex flex-col items-center md:block'>
+        <div className=' flex flex-col items-center md:block'>
           <div className='flex gap-6 mb-7'>
             <a href="#" className='w-10 h-10 flex justify-center items-center rounded-full hover:bg-gray-100 shadow-md'>
               <Image src={facebook} alt="facebook icon" />
@@ -53,12 +53,12 @@ const Footer = () => {
           <div>
             <p>Discover our App</p>
           </div>
-          <div className='flex gap-2 mt-4'>
-            <a href="#" className='flex gap-1 bg-black rounded-2xl py-2 px-4 hover:bg-gray-800'>
+          <div className='flex gap-2 mt-4 flex-wrap'>
+            <a href="#" className='flex-none w-1/2 flex gap-1 bg-black rounded-2xl py-2 px-4 hover:bg-gray-800'>
               <Image src={google_play} alt="Google play icon" />
               <Image src={get_it_on_google_play} alt="Google play description" />
             </a>
-            <a href="#" className='flex gap-1 bg-black rounded-2xl py-2 px-4 hover:bg-gray-800'>
+            <a href="#" className='flex-none w-1/2 flex gap-1 bg-black rounded-2xl py-2 px-4 hover:bg-gray-800'>
               <Image src={apple} alt="Apple icon" />
               <Image src={available_in_apple_store} alt="Apple description" />
             </a>
