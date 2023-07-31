41171b37333836a14bc9fbd7f8fb049421821892
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-31 16:21:26 +0800

    style: update mobile responsiveness destinations

--- components/destinations/Destinations.tsx
+++ components/destinations/Destinations.tsx
@@ -8,9 +8,9 @@ const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ['latin
 
 const Destinations = () => {
   return (
-    <div className="text-center mt-28 pb-20 debig">
-      <h3 className={`uppercase text-[#5E6282] text-lg font-semibold ${poppins.className}`}>Top Selling</h3>
-      <h2 className={`text-5xl font-bold text-[#14183E] mt-4 mb-14 ${volkhov.className}`}>Top Destinations</h2>
+    <div className="text-center mt-14 md:mt-28 pb-10 md:pb-20">
+      <h3 className={`text-[#5E6282] text-lg font-semibold ${poppins.className}`}>Top Selling</h3>
+      <h2 className={`text-2xl md:text-5xl font-bold text-[#14183E] mt-4 mb-14 ${volkhov.className}`}>Top Destinations</h2>
       <div className='grid grid-cols-1 md:grid-cols-3 gap-12'>
         {
           destinations.map((destination, index) => (
