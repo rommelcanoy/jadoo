b8807c79444adae99502f2415aedf8a0be580af0
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-31 16:21:33 +0800

    style: update mobile responsiveness categories

--- components/categories/Categories.tsx
+++ components/categories/Categories.tsx
@@ -8,10 +8,10 @@ const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ['latin
 
 const Categories = () => {
   return (
-    <div className="text-center mt-28 pb-20">
+    <div className="text-center mt-14 md:mt-28 pb-10 md:pb-20">
       <h3 className={`uppercase text-[#5E6282] text-lg font-semibold ${poppins.className}`}>Category</h3>
       <h2 className={`text-2xl md:text-5xl font-bold text-[#14183E] mt-4 mb-8 md:mb-14 ${volkhov.className}`}>We Offer Best Services</h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12'>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-12'>
         {
           categories.map((category, index) => (
             <Category key={index} category={category} />
