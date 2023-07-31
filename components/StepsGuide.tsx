4820c7d1a1a8b59112a0d707f9b28dd9fe0b7dda
Rommel Canoy <48854015+rommelcanoy@users.noreply.github.com>
2023-07-31 16:21:09 +0800

    style: update mobile responsiveness steps guide

--- components/StepsGuide.tsx
+++ components/StepsGuide.tsx
@@ -8,12 +8,12 @@ const poppins = Poppins({ weight: ['400', '500', '600', '700'], subsets: ['latin
 
 const StepsGuide = () => {
   return (
-    <div className='flex mb-48 gap-14 justify-around mt-24'>
-      <div className=''>
+    <div className='flex flex-col md:flex-row mb-24 md:mb-48 gap-14 justify-around mt-24'>
+      <div className='text-center md:text-left'>
         <h3 className={`text-[#5E6282] text-lg font-semibold ${poppins.className}`}>Easy and Fast</h3>
-        <h2 className={`text-5xl font-bold text-[#14183E] mt-4 mb-14 ${volkhov.className}`}>Book your next trip<br />in 3 easy steps</h2>
+        <h2 className={`text-2xl md:text-5xl font-bold text-[#14183E] mt-4 mb-14 ${volkhov.className}`}>Book your next trip<br />in 3 easy steps</h2>
         <div className='flex flex-col gap-12'>
-          <div className="flex gap-5 w-[395px]">
+          <div className="flex flex-col md:flex-row gap-5 md:w-[395px] items-center md:items-start">
             <div className='w-12 h-12 rounded-xl flex justify-center items-center bg-[#F0BB1F] flex-none'>
               <Image src={choose_destination} alt="Choose destination icon" />
             </div>
@@ -22,7 +22,7 @@ const StepsGuide = () => {
               <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus. </p>
             </div>
           </div>
-          <div className="flex gap-5 w-[395px]">
+          <div className="flex flex-col md:flex-row gap-5 md:w-[395px] items-center md:items-start">
             <div className='w-12 h-12 rounded-xl flex justify-center items-center bg-[#F15A2B] flex-none'>
               <Image src={choose_destination} alt="Choose destination icon" />
             </div>
@@ -31,7 +31,7 @@ const StepsGuide = () => {
               <p className='font-normal'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Urna, tortor tempus. </p>
             </div>
           </div>
-          <div className="flex gap-5 w-[395px]">
+          <div className="flex flex-col md:flex-row gap-5 md:w-[395px] items-center md:items-start">
             <div className='w-12 h-12 rounded-xl flex justify-center items-center bg-[#006380] flex-none'>
               <Image src={choose_destination} alt="Choose destination icon" />
             </div>
@@ -43,9 +43,9 @@ const StepsGuide = () => {
         </div>
       </div>
 
-      <div className='flex-1 flex justify-center'>
-        <div className='flex items-center relative w-[370px] h-full -mr-24'>
-          <div className='rounded-3xl shadow-xl p-6 flex-none bg-white z-10'>
+      <div className='flex-1 flex flex-col md:flex-row justify-center'>
+        <div className='flex items-center relative max-w-[370px] h-full md:-mr-24 w-full flex-1 justify-center'>
+          <div className='rounded-3xl shadow-xl p-6 flex-none bg-white z-10 w-[300px] md:w-full'>
             <Image src={trip_to_greece} alt="Trip to greece picture" className='rounded-3xl w-full h-40 mb-6 object-cover' />
             <div className='flex flex-col gap-6'>
               <div>
@@ -73,7 +73,7 @@ const StepsGuide = () => {
                 <Image src={heart} alt="heart icon" />
               </div>
             </div>
-            <div className='shadow-xl rounded-2xl flex z-10 bg-white bottom-[110px] absolute -right-36 w-[300px] p-6 gap-3'>
+            <div className='shadow-xl rounded-2xl hidden md:flex z-10 bg-white bottom-[-80px] border -right-20 md:bottom-[110px] absolute md:-right-36 md:w-[300px] p-6 gap-3'>
               <Image src={trip_to_rome} alt="Trip to Rome image" className='w-12 h-12 rounded-full' />
               <div className='w-full'>
                 <h5 className='font-medium text-sm text-[#84829A] mb-1'>Ongoing</h5>
@@ -87,7 +87,7 @@ const StepsGuide = () => {
               </div>
             </div>
           </div>
-          <Image src={blue_orb} alt="blue orb" className='absolute -top-5 -right-20' />
+          <Image src={blue_orb} alt="blue orb" className='absolute md:-top-5 md:-right-20 hidden md:block' />
         </div>
       </div>
     </div>
